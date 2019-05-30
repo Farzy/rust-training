371f476100bd799cf0b24f7ae212449fe45a322c
Farzad FARID <farzy@farzy.org>
2019-05-30 18:51:45 +0200

    hash maps and iter/collect

--- src/hashmaps.rs
+++ src/hashmaps.rs
@@ -7,4 +7,25 @@ pub fn main() {
     scores.insert(String::from("Yellow"), 50);
 
     println!("scores = {:#?}", scores);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+    println!("Zip = {:?}", teams.iter().zip(initial_scores.iter()));
+    let scores : HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+    println!("scores zip = {:?}", scores);
+
+    let a = [1, 2, 3];
+    let inverse : Vec<_> = a.iter()
+        .rev()
+        .map(|&x| 1.0 / f64::from(x))
+        .collect();
+
+    println!("rev.inverse({:?}) = {:?}", a, inverse);
+
+    let sc : Vec<_> = scores.iter()
+        .map(|(&x,&y)| {println!("Map {} -> {}", x, y); (x, *y+10) })
+        .collect();
+    for (x, y) in sc {
+        println!("For {} -> {}", x, y);
+    }
 }
