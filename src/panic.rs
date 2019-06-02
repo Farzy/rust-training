81e9fcfc1dea31ff85bd115fb24bf5d425ce3c24
Farzad FARID <farzy@farzy.org>
2019-06-02 17:03:51 +0200

    Add expect()

--- src/panic.rs
+++ src/panic.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::ErrorKind;
 
 pub fn main() {
-    let f = File::open("hello.txt").unwrap_or_else(|error| {
+    let _f = File::open("hello.txt").unwrap_or_else(|error| {
         if error.kind() == ErrorKind::NotFound {
             File::create("hello.txt").unwrap_or_else(|error| {
                 panic!("Tried to create a file but there was a problem: {:?}", error);
@@ -11,4 +11,6 @@ pub fn main() {
             panic!("There was a problem opening the file: {:?}", error);
         }
     });
+
+//    let _f = File::open("hello2.txt").expect("Failed to open hello.txt");
 }
