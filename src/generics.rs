da1f1478aaad0ac97e59cf6232ec68a670d76012
Farzad FARID <farzy@farzy.org>
2019-06-06 23:00:54 +0200

    Traits

--- src/generics.rs
+++ src/generics.rs
@@ -56,6 +56,36 @@ impl<T: Copy> Point<T> {
     }
 }
 
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
 pub fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
@@ -97,5 +127,16 @@ pub fn main() {
     println!("s.x = {}", s.x());
     println!("s.x (Copy version) = {}", s.x2());
     assert_eq!(s.x(), &s.x2());
+
+    //------------------------------------------------------------
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
 }
 
