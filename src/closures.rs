a609b9e5a620c1a47424e731642ed0ad1ac6cf7b
Farzad FARID <farzy@farzy.org>
2019-06-16 22:59:51 +0200

    Closures: closure with cache

--- src/closures.rs
+++ src/closures.rs
@@ -8,26 +8,54 @@ pub fn main() {
     );
 }
 
+struct Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
+}
 
 use std::thread;
 use std::time::Duration;
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_result = |num| {
+    let mut expensive_result = Cacher::new(|num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         intensity
-    };
+    });
 
 
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
-            expensive_result(intensity)
+            expensive_result.value(intensity)
         );
         println!(
             "Next, do {} situps!",
-            expensive_result(intensity)
+            expensive_result.value(intensity)
         );
     } else {
         if random_number == 3 {
@@ -35,7 +63,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_result(intensity)
+                expensive_result.value(intensity)
             );
         }
     }
