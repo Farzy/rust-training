4a45a679dc6885c05faec969640984ea4b2311ad
Farzad FARID <farzy@farzy.org>
2019-06-20 00:04:27 +0200

    Closures: closure with generic parameters

--- src/closures.rs
+++ src/closures.rs
@@ -79,3 +79,78 @@ fn call_with_different_values() {
 
     assert_eq!(v2, 2);
 }
+
+// ----------------------------------------------------------------------------------------------
+
+use std::hash::Hash;
+
+struct CacherGen<T, U, V>
+    where T: Fn(U) -> V
+{
+    calculation: T,
+    value: HashMap<U,V>,
+}
+
+impl<T,U,V> CacherGen<T,U,V>
+    where T: Fn(U) -> V,
+          U: Eq + Hash + Copy,
+          V: Copy
+{
+    fn new(calculation: T) -> CacherGen<T,U,V> {
+        CacherGen {
+            calculation,
+            value: HashMap::new(),
+        }
+    }
+
+    fn value(&mut self, arg: U) -> V {
+        match self.value.get(&arg) {
+            Some(&v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value.insert(arg, v);
+                v
+            },
+        }
+    }
+}
+
+fn generate_workout_gen(intensity: &str, random_number: f64) {
+    let mut expensive_result = CacherGen::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        let i : u64 = intensity.parse().unwrap();
+        i
+    });
+
+
+    if intensity == "25" {
+        println!(
+            "Today, do {} pushups!",
+            expensive_result.value(intensity)
+        );
+        println!(
+            "Next, do {} situps!",
+            expensive_result.value(intensity)
+        );
+    } else {
+        if random_number == 3.0 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result.value(intensity)
+            );
+        }
+    }
+}
+
+#[test]
+fn call_with_different_values_gen() {
+    let mut c = CacherGen::new(|a : &str| -> u64 { a.parse().unwrap() });
+
+    let v1 = c.value("1");
+    let v2 = c.value("2");
+
+    assert_eq!(v2, 2);
+}
