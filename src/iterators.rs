5d9995c1772a4f99e6a485a6e97d21b81081554d
Farzad FARID <farzy@farzy.org>
2019-06-23 19:04:34 +0200

    Fix iterator, add test

--- src/iterators.rs
+++ src/iterators.rs
@@ -82,6 +82,8 @@ impl Iterator for Counter {
     type Item = u32;
 
     fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
         if self.count < 6 {
             Some(self.count)
         } else {
@@ -100,4 +102,13 @@ fn calling_next_directly() {
     assert_eq!(counter.next(), Some(4));
     assert_eq!(counter.next(), Some(5));
     assert_eq!(counter.next(), None);
+}
+
+#[test]
+fn using_other_iterator_trait_methods() {
+    let sum: u32 = Counter::new().zip(Counter::new().skip(1))
+                                 .map(|(a, b)| a * b)
+                                 .filter(|x| x % 3 == 0)
+                                 .sum();
+    assert_eq!(18, sum);
 }
