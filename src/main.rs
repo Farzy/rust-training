79b1f0435bb197365fb552401a3b228a325a6637
Farzad FARID <farzy@farzy.org>
2020-04-13 17:28:29 +0200

    Add function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
-fn main() {
-    say_hello("world")
-}
-
 fn say_hello(name: &str) {
     println!("Hello {}!", name)
+}
+
+fn add(a: i32, b: i64) -> i32 {
+    return a + (b as i32);
+}
+
+fn main() {
+    say_hello("world");
+
+    let i1 = 1;
+    let i2 = 2;
+    println!("{} + {} = {}", i1, i2, add(i1, i2));
 }
