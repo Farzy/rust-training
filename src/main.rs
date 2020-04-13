fc39cedca7edb038ff2eba9924f3df6334988070
Farzad FARID <farzy@farzy.org>
2020-04-13 17:39:31 +0200

    LinkedList and loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::LinkedList;
+
 fn say_hello(name: &str) {
     println!("Hello {}!", name)
 }
@@ -12,4 +14,17 @@ fn main() {
     let i1 = 1;
     let i2 = 2;
     println!("{} + {} = {}", i1, i2, add(i1, i2));
+
+    let mut ll = LinkedList::new();
+
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(4);
+
+    println!("Print LL using debug syntax:");
+    println!("ll = {:?}", ll);
+    println!("Print LL using loop:");
+    for foo in ll {
+        println!("{}", foo);
+    }
 }
