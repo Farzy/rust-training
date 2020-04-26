b10d92567c3fa1990832f1162269c98d2cc8b601
Farzad FARID <farzy@farzy.org>
2020-04-26 19:00:10 +0200

    Add commands line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::{LinkedList, HashMap};
-use std::env;
+use std::{env, io};
 mod hello;
 use crate::hello::hello;
 mod strings;
@@ -168,4 +168,25 @@ fn main() {
 
     section("Randomness");
     random();
+
+    section("Input");
+    // Only execute this interactive code if "-i" is provided on the command line
+    let mut do_input = false;
+    for arg in env::args() {
+        if arg == "-i" {
+            do_input = true;
+            break;
+        }
+    }
+    if do_input {
+        let mut word = String::new();
+        while word.trim() != "rust" {
+            println!("What is the secret word?");
+            word = String::new();
+            io::stdin().read_line(&mut word).expect("Could not read stdin");
+        }
+        println!("You found the secret word! Please proceed!");
+    } else {
+        println!("SKIPPED: In order to execute this section add '-i' to the command line.")
+    }
 }
