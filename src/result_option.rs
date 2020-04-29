3c896448e65eef3d9afffa40cb2a256cb3151cda
Farzad FARID <farzad.farid@kapten.com>
2020-04-29 22:11:53 +0200

    Map over Result<>

--- src/result_option.rs
+++ src/result_option.rs
@@ -1,3 +1,5 @@
+use std::num::ParseIntError;
+
 pub fn main() {
     println!("Convert Result<> to Option:");
     let r1 = "42".parse::<i32>();
@@ -7,7 +9,7 @@ pub fn main() {
     let o2 = r2.ok();
     println!("Options: {:?} / {:?}'", o1, o2);
 
-    println!("Convert Option to Result<>:");
+    println!("\nConvert Option to Result<>:");
     let o1: Option<i32> = Some(42);
     let o2: Option<i32> = None;
     println!("Options: {:?} / {:?}", o1, o2);
@@ -15,7 +17,7 @@ pub fn main() {
     let r2 = o2.ok_or("Not a number");
     println!("Results: '{:?}' / '{:?}'", r1, r2);
 
-    println!("Default values on Result<>:");
+    println!("\nDefault values on Result<>:");
     let r1 = "42".parse::<i32>();
     let r2 = "toto".parse::<i32>();
     println!("Results: '{:?}' / '{:?}", r1, r2);
@@ -23,7 +25,7 @@ pub fn main() {
     let i2 = r2.unwrap_or_default();
     println!("unwrap_or_default: {:?} / {:?}'", i1, i2);
 
-    println!("Default values on Option:");
+    println!("\nDefault values on Option:");
     let o1: Option<i32> = Some(42);
     let o2: Option<i32> = None;
     println!("Options: {:?} / {:?}", o1, o2);
@@ -31,8 +33,15 @@ pub fn main() {
     let i2 = o2.unwrap_or_default();
     println!("Results: '{:?}' / '{:?}'", i1, i2);
 
+    println!("\nMap over Option:");
     let a = vec![Some(2), Some(-3), None, Some(10)];
     println!("Array of Option: {:?}", a);
     let b: Vec<Option<i32>> = a.iter().map(|x| x.map(|y| y*y)).collect();
     println!("Array of Option squared: {:?}", b);
+
+    println!("\nMap over Result<>:");
+    let s = vec!["2", "32", "toto", "-10"];
+    println!("Array of strings: {:?}", s);
+    let numbers: Vec<Result<i32, ParseIntError>> = s.iter().map(|x| x.parse::<i32>()).collect();
+    println!("Array of Result<i32>: {:?}", numbers);
 }
