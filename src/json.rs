b0fd586e56eede856b5ddf1d7dc3f545ab55ebfe
Farzad FARID <farzad.farid@kapten.com>
2020-04-29 20:32:33 +0200

    Implement Default on Person and use it in unwrap_or_default()

--- src/json.rs
+++ src/json.rs
@@ -3,6 +3,12 @@ struct Person {
     name: String,
 }
 
+impl Default for Person {
+    fn default() -> Self {
+        Person { name: "Unknown person implemented as default".to_string() }
+    }
+}
+
 pub fn main() {
     // Broken JSON
     let broken_json = serde_json::from_str::<Person>(r#"{
@@ -17,6 +23,14 @@ pub fn main() {
     // Next version is optimized
     let first_inner = broken_json
         .unwrap_or(Person { name: String::from("Unknown person") });
+    println!("First name: {:?}", first_inner.name);
+    // Third version use default value
+    // NOTE: unwrap_or() takes ownership of the content of Result≤>, adding "&" does not
+    //       solve the problem. So let's create the JSON again
+    let broken_json = serde_json::from_str::<Person>(r#"{
+            "name": "Margaret Hamilton",
+        }"#);
+    let first_inner = broken_json.unwrap_or_default();
     println!("First name: {:?}", first_inner.name);
 
     // Valid JSON
