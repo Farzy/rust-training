af23f43ff2734ea98ddb4e5dbb8a1dddb99190f5
Farzad FARID <farzad.farid@kapten.com>
2020-04-29 20:25:29 +0200

    Remove match() on Result<>, optimize with unwrap_or()

--- src/json.rs
+++ src/json.rs
@@ -1,24 +1,35 @@
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 struct Person {
     name: String,
 }
 
 pub fn main() {
+    // Broken JSON
     let broken_json = serde_json::from_str::<Person>(r#"{
             "name": "Margaret Hamilton",
         }"#);
-    let first_inner = match broken_json {
-        Ok(inner) => inner,
-        Err(e) => Person { name: String::from(format!("unknown ({})", e)) },
+    // First version with match() on Result<>
+    let first_inner = match &broken_json {
+        Ok(inner) => inner.clone(),
+        Err(e) => Person { name: String::from(format!("unknown ({})", *e)) },
     };
     println!("First name: {:?}", first_inner.name);
+    // Next version is optimized
+    let first_inner = broken_json
+        .unwrap_or(Person { name: String::from("Unknown person") });
+    println!("First name: {:?}", first_inner.name);
 
+    // Valid JSON
     let first = serde_json::from_str::<Person>(r#"{
             "name": "Margaret Hamilton"
         }"#);
-    let first_inner = match first {
-        Ok(inner) => inner,
-        Err(e) => Person { name: String::from(format!("unknown ({})", e)) },
+    // First version with match() on Result<>
+    let first_inner = match &first {
+        Ok(inner) => inner.clone(),
+        Err(e) => Person { name: String::from(format!("unknown ({})", *e)) },
     };
+    println!("First name: {:?}", first_inner.name);
+    // Next version is optimized
+    let first_inner = first.unwrap_or(Person { name: String::from("Unknown person") });
     println!("First name: {:?}", first_inner.name);
 }
