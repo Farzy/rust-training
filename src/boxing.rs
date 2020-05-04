a3af9d5e65076a124d547cdbdacd800a79ab2450
Farzad FARID <farzy@farzy.org>
2020-05-04 23:40:53 +0200

    Implement our own Smart Pointer MyBox

--- src/boxing.rs
+++ src/boxing.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 #[derive(Debug)]
 enum List {
     Cons(i32, Box<List>),
@@ -6,10 +8,31 @@ enum List {
 
 use List::*;
 
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.0
+    }
+}
+
 pub fn main() {
     let b = Box::new(5);
     println!("Box<u32> = {}", b);
 
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     println!("List = {:?}", list);
+
+    let x = 5;
+    let y = MyBox::new(x);
+    println!("Asserting that *MyBox::new(x) == x");
+    assert_eq!(5, x);
+    assert_eq!(x, *y);
 }
