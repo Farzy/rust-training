9cfb0e02865b21ffa7843d087e5620079f99880e
Farzad FARID <farzy@farzy.org>
2020-05-05 23:41:08 +0200

    Add Rc

--- src/smart_pointers.rs
+++ src/smart_pointers.rs
@@ -1,6 +1,8 @@
 use std::ops::Deref;
 use rust_training::helper;
 
+// --------------------------------------------------------------------------------------------
+
 #[derive(Debug)]
 enum List {
     Cons(i32, Box<List>),
@@ -16,6 +18,8 @@ fn boxing() {
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
     println!("List = {:?}", list);
 }
+
+// --------------------------------------------------------------------------------------------
 
 struct MyBox<T>(T);
 
@@ -39,6 +43,8 @@ fn deref() {
     assert_eq!(5, x);
     assert_eq!(x, *y);
 }
+
+// --------------------------------------------------------------------------------------------
 
 struct CustomSmartPointer {
     data: String,
@@ -69,6 +75,44 @@ fn dropper() {
     println!("At this point it is dropped.");
 }
 
+// --------------------------------------------------------------------------------------------
+
+use std::rc::Rc;
+
+#[derive(Debug)]
+enum ListRc<T> {
+    ConsRc(T, Rc<ListRc<T>>),
+    NilRc,
+}
+
+use self::ListRc::*;
+
+fn reference_counter() {
+    // This won't compile
+    // let a = Cons(5, Box::new(Cons(10, Box::new(Nil))));
+    // let b = Cons(3, Box::new(a));
+    // let c = Cons(3, Box::new(a));
+
+    let a = Rc::new(ConsRc(5, Rc::new(ConsRc(10, Rc::new(NilRc)))));
+    println!("a = {:?}", a);
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = ConsRc(3, Rc::clone(&a));
+    println!("b = {:?}", b);
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = ConsRc(3, Rc::clone(&a));
+        println!("c = {:?}", c);
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    println!("Dropping b");
+    drop(b);
+    println!("count after b is dropped = {}", Rc::strong_count(&a));
+
+}
+
+// --------------------------------------------------------------------------------------------
+
 pub fn main() {
     helper::subsection("Box type");
     boxing();
@@ -78,4 +122,7 @@ pub fn main() {
 
     helper::subsection("Drop trait");
     dropper();
+
+    helper::subsection("RC type");
+    reference_counter();
 }
