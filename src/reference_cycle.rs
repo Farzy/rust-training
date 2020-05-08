e9403208befe9c9d09e7b0471807a882ab7592d7
Farzad FARID <farzy@farzy.org>
2020-05-08 15:55:26 +0200

    Add Graph structure with Weak reference

--- src/reference_cycle.rs
+++ src/reference_cycle.rs
@@ -1,6 +1,6 @@
 use rust_training::helper;
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
 #[derive(Debug)]
 enum List {
@@ -44,7 +44,38 @@ fn reference_cycle() {
     // println!("a next item = {:?}", a.tail());
 }
 
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
+fn weak_ref() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    let branch = Rc::new(Node {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!("leaf = {:?}", leaf);
+}
+
 pub fn main() {
     helper::subsection("Reference cycle");
     reference_cycle();
+
+    helper::subsection("Using Weak Ref");
+    weak_ref();
 }
