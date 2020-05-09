a860e152c0aac896cd2cd611e65a564a813ac114
Farzad FARID <farzad.farid@kapten.com>
2020-05-09 21:29:19 +0200

    Create a extern Rust function (not called, C needed)

--- src/unsafe_rust.rs
+++ src/unsafe_rust.rs
@@ -92,3 +92,8 @@ fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
 extern "C" {
     fn abs(input: i32) -> i32;
 }
+
+#[no_mangle]
+pub extern "C" fn call_from_c() {
+    println!("Just called a Rust function from C!");
+}
