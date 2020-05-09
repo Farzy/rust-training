61f9559bd15b7e8fc10a07b265d54a6fa3c181f1
Farzad FARID <farzy@farzy.org>
2020-05-09 22:49:48 +0200

    Default Generic Type Parameters and Operator Overloading

--- src/advanced_traits.rs
+++ src/advanced_traits.rs
@@ -17,10 +17,28 @@ impl Add for Point {
     }
 }
 
+#[derive(Debug, PartialEq)]
+struct Millimeters(u32);
+
+#[derive(Debug, PartialEq)]
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, rhs: Meters) -> Self::Output {
+        Millimeters(self.0 + (rhs.0 * 1000))
+    }
+}
 
 pub fn main() {
     assert_eq!(
         Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
         Point { x: 3, y: 3}
+    );
+
+    assert_eq!(
+        Millimeters(1500),
+        Millimeters(500) + Meters(1)
     );
 }
